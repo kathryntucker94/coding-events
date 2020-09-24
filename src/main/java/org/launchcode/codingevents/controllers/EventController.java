942217bee4cd6c0125ca16db7c87440d0dda4194
Kathryn Tucker <kathryn.tucker94@gmail.com>
2020-09-23 20:30:49 -0500

    Completed steps 11.6.6

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,6 +3,7 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,13 +14,24 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String EventController(Model model){
-       List<String> events = new ArrayList<>();
-       events.add("Code with Pride");
-       events.add("StrangeLoop");
-       events.add("LaunchCode Event");
        model.addAttribute("events", events);
        return "events/index";
+    }
+
+    //lives at /events/create
+    @GetMapping("create")
+    public String renderCreateEventForm(){
+        return "events/create";
+    }
+
+    //lives at /events/create
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+        events.add(eventName);
+        return "redirect:";
     }
 }
